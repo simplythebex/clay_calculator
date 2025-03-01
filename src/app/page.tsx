b5097e99750c23ad2a4ec1b496d4ddff0c390442
simplythebex <becca.jones@live.co.uk>
2025-03-01 19:48:12 +0000

    finished homepage design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,77 +2,65 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex items-center">
-          <h2>we do the maths, so you don’t have to</h2>
-        </div>
-        <div>
-          <p>First tell us your desired dimentions and clay details...</p>
-        </div>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <input type="text" placeholder="desired height (cm)" className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#F4ACB7]text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"></input>
-          <input type="text" placeholder="desired width (cm)" className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#F4ACB7]text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"></input>
-          <input type="text" placeholder="clay shrinkage %" className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#F4ACB7]text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"></input>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#F4ACB7] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            calculate! <svg aria-hidden="true" viewBox="0 0 24 24"></svg>
-          </a>
-        </div>
-      </main>
-
-
-
-
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+    <div className="min-h-screen">
+      <header className="sticky">
+        <nav className="min-w-screen mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8">
+          <a href="#">home</a>
+        </nav>
+      </header>
+      <div className="grid grid-rows-[10px_1fr_10px] items-center justify-items-center sm:p-20 min-h-full">
+        <main className="min-h-screen overflow-y-scroll flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <div className="flex items-center">
+            <h2 className="sm:text-[50px] text-[25px]">we do the maths, so that you don’t have to</h2>
+          </div>
+          <div>
+            <p>First tell us your desired dimentions and clay details...</p>
+          </div>
+          <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <input type="text" placeholder="desired height (cm)" className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#F4ACB7]text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"></input>
+            <input type="text" placeholder="desired width (cm)" className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#F4ACB7]text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"></input>
+            <input type="text" placeholder="clay shrinkage %" className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#F4ACB7]text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"></input>
+            <a
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#D8E2DC] text-background gap-2 hover:bg-[#8bb7a0] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              calculate! <svg aria-hidden="true" viewBox="0 0 24 24"></svg>
+            </a>
+          </div>
+        </main>
+      </div>
+      <footer className="row-start-3 flex gap-6 p-4 flex-wrap items-center justify-center bottom-0 sticky">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/simplythebex/clay_calculator"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="/github-mark.svg"
             alt="File icon"
             width={16}
             height={16}
           />
-          Learn
+          GitHub
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="/instagram.svg"
             alt="Window icon"
             width={16}
             height={16}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Instagram
         </a>
       </footer>
     </div>
