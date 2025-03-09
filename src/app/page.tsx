ea256b02d72aa32684e38be88e441f73f0711a55
simplythebex <becca.jones@live.co.uk>
2025-03-09 11:15:29 +0000

    add seperate width and height calculations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,24 @@ import Image from "next/image";
 import Link from 'next/link'
 
 export default function Home() {
-  const [desiredSize, setDesiredSize] = useState<number | "">("");
+  const [desiredWidth, setDesiredWidth] = useState<number | "">("");
+  const [desiredHeight, setDesiredHeight] = useState<number | "">("");
+  const [desiredDepth, setDesiredDepth] = useState<number | "">("");
   const [shrinkagePercentage, setShrinkagePercentage] = useState<number | "">("");
-  const [result, setResult] = useState<number | null>(null);
+  const [result, setResult] = useState<{ width: number; height: number;} | null>(null);
 
   const calculateShrinkage = (event: React.FormEvent) => {
     event.preventDefault();
 
-    if (desiredSize === "" || shrinkagePercentage === "") return;
+    if (desiredWidth === "" || desiredHeight === "" || shrinkagePercentage === "") return;
 
-    const calculatedSize = desiredSize / (1 - shrinkagePercentage / 100);
-    setResult(calculatedSize);
+    const calculatedWidth = (desiredWidth / (1 - shrinkagePercentage / 100)).toFixed(1);
+    const calculatedHeight = (desiredHeight / (1 - shrinkagePercentage / 100)).toFixed(1);
+
+    setResult({
+      width: calculatedWidth,
+      height: calculatedHeight
+    });
   };
 
   return (
@@ -27,23 +34,40 @@ export default function Home() {
 
       <div className="grid grid-rows-[10px_1fr_10px] items-center justify-items-center sm:p-20 min-h-full">
         <main className="min-h-screen overflow-y-scroll flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <div className="flex items-center flex-col">
+          <div className="flex flex-col">
             <h2 className="sm:text-[50px] text-[25px]">Clay Shrinkage Calculator</h2>
-            <p className="pt-8">First, tell us your desired dimensions and clay details...</p>
+            <p className="pt-8 sm:text-[15px] sm:m-5">First, tell us your desired dimensions and clay details...</p>
           </div>
 
           {/* Form */}
-          <form onSubmit={calculateShrinkage} className="flex gap-4 items-center flex-col sm:flex-row">
+          <form onSubmit={calculateShrinkage} className="flex gap-4 flex-col sm:flex-row">
+            <div className="flex items-center gap-2">
             <input
-              name="desiredSize"
-              id="desiredSize"
+              name="desiredWidth"
+              id="desiredWidth"
               type="number"
-              placeholder="desired size (cm)"
+              placeholder="desired width (cm)"
               required
-              value={desiredSize}
-              onChange={(e) => setDesiredSize(Number(e.target.value) || "")}
+              value={desiredWidth}
+              onChange={(e) => setDesiredWidth(Number(e.target.value) || "")}
               className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#F4ACB7] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             />
+            <p>cm wide</p>
+            </div>
+            <div className="flex items-center gap-2">
+            <input
+              name="desiredHeight"
+              id="desiredHeight"
+              type="number"
+              placeholder="desired height (cm)"
+              required
+              value={desiredHeight}
+              onChange={(e) => setDesiredHeight(Number(e.target.value) || "")}
+              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#F4ACB7] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            />
+            <p>cm high</p>
+            </div>
+            <div className="flex items-center gap-2">
             <input
               name="shrinkagePercentage"
               id="shrinkagePercentage"
@@ -54,6 +78,8 @@ export default function Home() {
               onChange={(e) => setShrinkagePercentage(Number(e.target.value) || "")}
               className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#F4ACB7] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             />
+            <p>shrinkage %</p>
+            </div>
             <button
               type="submit"
               className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#D8E2DC] text-background gap-2 hover:bg-[#8bb7a0] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -64,8 +90,8 @@ export default function Home() {
 
           {/* Result Display */}
           {result !== null && (
-            <div className="mt-4 p-4 bg-[#F4ACB7] rounded-lg">
-              <p>You need to throw a shape that measures <strong>{result.toFixed(2)} cm</strong></p>
+            <div className="sm:m-5 p-4 bg-[#F4ACB7] rounded-lg">
+              <p>You need to throw a shape that measures <strong>{result.height}cm high and {result.width}cm wide.</strong></p>
             </div>
           )}
         </main>
